7e503f51d8b0e0d8d1f1eed0f0962deb297bfc9a
ritsea17 <ritter662.s@gmail.com>
2021-10-01 11:41:16 +0200

    Inital Commit

---
+++ team_up_4_success/android/app/src/main/java/com/example/team_up_4_success/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.team_up_4_success;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ team_up_4_success/ios/Runner/AppDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Flutter
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
